d53c920117b0c15aa4074ad0a1323f44e17085b8
kathsydoruk <kathsydoruk@gmail.com>
2026-01-14 02:10:18 +0200

    L26 ENV configs creation

--- playwright.config.js
+++ playwright.config.js
@@ -5,9 +5,13 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({
+  path: process.env.ENV
+    ? path.resolve(process.cwd(), `.env.${process.env.ENV}`)
+    : path.resolve(process.cwd(), ".env"),
+});
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -27,10 +31,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "https://qauto.forstudy.space/",
+    baseURL: process.env.BASE_URL,
     httpCredentials: {
-      username: "guest",
-      password: "welcome2qauto",
+      username: process.env.AUTH_CREDENTIALS_USERNAME ?? "",
+      password: process.env.AUTH_CREDENTIALS_PASSWORD ?? "",
     },
     //https://example.cypress.io/
     //https://qauto.forstudy.space/
